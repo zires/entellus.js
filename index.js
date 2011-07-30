6a1fdf9c862e939ed78c4d3cea163227c7142008
zires <zshuaibin@gmail.com>
2011-07-30 17:46:13 +0800

    Add: makefile for expresso and index.js for package.json add entellus file and test file

---
+++ index.js
@@ -0,0 +1,7 @@
+/*!
+* entellus.js <chat server based on node.js and socket.io>
+* zires <zshuaibin@gmail.com>
+* MIT Licensed
+*/
+
+module.exports = require('./lib/entellus');
---
+++ test/entellus.test.js
@@ -0,0 +1,20 @@
+/*!
+* entellus.js <chat server based on node.js and socket.io>
+* zires <zshuaibin@gmail.com>
+* MIT Licensed
+*/
+
+/*
+ * Preparation dependencies
+ */
+var entellus = require('entellus'),
+	should   = require('should');
+	
+/**
+ * Test
+ */	
+module.exports = {
+	'it can run default host and port without arguments': function () {
+	}
+}
+	
