44a5b8541260a4ae772149e9368e24bbd51f0369
zires <zshuaibin@gmail.com>
2011-08-01 08:33:26 +0800

    Add: test suite

--- test/entellus.test.js
+++ test/entellus.test.js
@@ -8,13 +8,22 @@
  * Preparation dependencies
  */
 var entellus = require('entellus'),
-	should   = require('should');
+	  should   = require('should'),
+    socket   = require('socket.io'),
+    http     = require('http');
 	
 /**
  * Test
  */	
 module.exports = {
-	'it can run default host and port without arguments': function () {
-	}
+	'it should have a property named socket': function(){
+    entellus.should.have.property('socket', socket);
+  },
+  
+  'test listen function should return a http server': function(){
+    var io = entellus.listen(3000);
+    io.should.be.an.instanceof(socket.Manager);
+    io.server.should.be.an.instanceof(http.Server);
+    io.server.close();
+  }
 }
-	
